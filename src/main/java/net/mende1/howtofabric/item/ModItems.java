2ef8fdcee5b50a5a3b8fc7c74946b00793237541
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2023-04-28 21:45:43 -0300

    updated mod to 1.19.3 and change item to Citrine

--- src/main/java/net/mende1/howtofabric/item/ModItems.java
+++ src/main/java/net/mende1/howtofabric/item/ModItems.java
@@ -1,31 +1,44 @@
 package net.mende1.howtofabric.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.mende1.howtofabric.HowToFabric;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item RAW_TANZANITE = registerItem(
-            "raw_tanzanite",
-            new Item(new Item.Settings()),
-            ModItemGroup.TANZANITE
+    public static final Item RAW_CITRINE = registerItem(
+            "raw_citrine",
+            new Item(new FabricItemSettings())
     );
-    public static final Item TANZANITE = registerItem(
-            "tanzanite",
-            new Item(new Item.Settings()),
-            ModItemGroup.TANZANITE
+    public static final Item CITRINE = registerItem(
+            "citrine",
+            new Item(new FabricItemSettings())
     );
 
-    private static Item registerItem(String name, Item item, ItemGroup group) {
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.INGREDIENTS, CITRINE);
+        addToItemGroup(ItemGroups.INGREDIENTS, RAW_CITRINE);
+
+        addToItemGroup(ModItemGroup.CITRINE, CITRINE);
+        addToItemGroup(ModItemGroup.CITRINE, RAW_CITRINE);
+    }
+
+    public static void addToItemGroup(ItemGroup group, Item item) {
         ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(HowToFabric.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         HowToFabric.LOGGER.debug("Registering Mod Items for " + HowToFabric.MOD_ID);
+
+        addItemsToItemGroup();
     }
 }
