53938f8538e906654e7a4856b5058846aafc0c2e
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2023-04-29 23:56:36 -0300

    updated json config files with fabric Datagen feat

--- src/main/java/net/mende1/howtofabric/data/ModLootTableGenerator.java
+++ src/main/java/net/mende1/howtofabric/data/ModLootTableGenerator.java
@@ -14,7 +14,7 @@ public class ModLootTableGenerator extends FabricBlockLootTableProvider {
     public void generate() {
         addDrop(ModBlocks.CITRINE_BLOCK);
 
-        oreDrops(ModBlocks.CITRINE_ORE, ModItems.RAW_CITRINE);
-        oreDrops(ModBlocks.DEEPSLATE_CITRINE_ORE, ModItems.RAW_CITRINE);
+        addDrop(ModBlocks.CITRINE_ORE, oreDrops(ModBlocks.CITRINE_ORE, ModItems.RAW_CITRINE));
+        addDrop(ModBlocks.DEEPSLATE_CITRINE_ORE, oreDrops(ModBlocks.DEEPSLATE_CITRINE_ORE, ModItems.RAW_CITRINE));
     }
 }
